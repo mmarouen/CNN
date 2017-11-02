b65f9013a5e9a00bbe373ab67981ac3129c6fac8
azzouz marouen <azzouz.marouen@gmail.com>
2017-11-02 18:41:33 +0100

    Create gradientDescent.R

---
+++ GradientDescent/gradientDescent.R
@@ -0,0 +1,23 @@
+gradientDescent<-function(Im,respMat,W,archTab,learning_rate){
+  r=1 #epochs
+  t=0 #iterations
+  obj=c()
+  plot.new()
+  
+  repeat{
+    fp=FP(Im,W,archTab,TRUE)
+    M=fp$M
+    Y=fp$Y
+    bp=BP(Y,M,W,resp,archi,learning_rate)
+    ypred=Y[[length(Y)]]
+    W=bp$W
+    obj1=Objective(ypred,respMat)
+    obj=c(obj,obj1)
+    print(obj1)
+    # lines( 1:r,obj)
+    # Sys.sleep(0)
+    print(r)
+    r=r+1
+    if(r==100) break
+  }
+}
