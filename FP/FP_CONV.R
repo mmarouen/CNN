43cf6ee19f5613dd65b6f1e18f177523258d750d
azzouz marouen <azzouz.marouen@gmail.com>
2017-11-08 22:43:49 +0100

    Update FP_CONV.R

--- FP/FP_CONV.R
+++ FP/FP_CONV.R
@@ -1,20 +1,28 @@
 #performs forward propagation for convolution layer
 
-FP_Conv<-function(Im,W,p1,p2,zeroPad){
+FP_Conv<-function(Im, #input images
+                  W, #weights
+                  p1, #horizontal stride
+                  p2, #vertical stride
+                  zeroPad #binary
+                 ){
   
-  im2col=Im2Col(Im,W,p1,p2,zeroPad)
+  im2col=Im2Col(Im,W$Weight,p1,p2,zeroPad)
   X=im2col$X
   z1=im2col$z1
   z2=im2col$z2
   Wl=apply(W$Weight,4,c)
   bl=W$bias
-  W=W$Weight
-  Z=apply(X,3,function(x) x%*%Wl+bl)
-  Z=array(Z,dim=c(z1*z2,dim(W)[4],dim(Im)[4]))
+  D=dim(W$Weight)[4]
+  N=dim(Im)[4]
+  Z=apply(X,3,function(x) t(t(x%*%Wl)+bl))
+  Z=array(Z,dim=c(z1*z2,D,N))
   Y=apply(Z,3,function(x){
     x[x<0]=0
     return(x)
   })
-  Y=array(Y,dim=c(z1,z2,dim(W)[4],dim(Im)[4]))
-  return(list(new_Im=Y,Z=Z))
+  Y=array(Y,dim=c(z1,z2,D,N))
+  Y=aperm(Y,c(2,1,3,4))
+  return(list(new_Im=Y))
 }
+
