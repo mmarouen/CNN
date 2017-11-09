177536e2fdb598f4c93b5b9cabc3d939fcb12448
azzouz marouen <azzouz.marouen@gmail.com>
2017-11-09 10:29:00 +0100

    Update BP_CONV.R

--- BP/BP_CONV.R
+++ BP/BP_CONV.R
@@ -1,6 +1,5 @@
 #BP on the conv layer
 #Computes deltas & gradients with regards to weights & biases
-#Implementation not yet properly vectorized, it's ongoing
 
 BP_Conv<-function(Y1,#activation of current layer (compute deltas)
                   Y2, #activation of previous layer (compute gradients)
@@ -40,26 +39,7 @@ BP_Conv<-function(Y1,#activation of current layer (compute deltas)
     deltas1=partial_Z*D1
   }
   if(!noWeight){#compute gradient
-    #deltas1f=flipKernel(deltas1)
-    d=dim(deltas1)[3]
-    grads=array(dim  =c(k1,k2,C,d))
-    Y11=apply(Y2,4,function(x)apply(x,3,function(t){
-      tt=cbind(0,t,0)
-      tt2=rbind(0,tt,0)
-      return(tt2)
-    }))
-    Y11=array(Y11,dim=c(dim(Y2)[1]+2,dim(Y2)[2]+2,dim(Y2)[3],dim(Y2)[4]))
-    n1=dim(Y1)[1]
-    n2=dim(Y1)[2]
-    for(i in 1:C){
-      for(j in 1:d){
-        for(kk1 in 1:3){
-          for(kk2 in 1:3){
-            grads[kk1,kk2,i,j]=sum(deltas1[,,j,]*Y11[kk1:(kk1+n1-1),kk2:(kk2+n2-1),i,])
-          }
-        }
-      }
-    }
+    grads1=convolveMat(Y2,deltas1,k1,k2)
     grads_b=apply(deltas1,3,sum)
   }
   return(list(grads=grads,grads_b=grads_b,deltas=deltas1))
