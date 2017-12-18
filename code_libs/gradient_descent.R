f19cd723df52cae212a629aa3ce9b9272b23b238
azzouz marouen <azzouz.marouen@gmail.com>
2017-12-18 15:33:58 +0100

    Update gradient_descent.R

--- code_libs/gradient_descent.R
+++ code_libs/gradient_descent.R
@@ -147,3 +147,178 @@ FP_Pool<-function(Im,mask,p1,p2,zeroPad){
   return(list(M=Maxes,Y=Y0))
 }
 
+#Performs BP on a set of images
+BP<-function(Y, #activations
+             M, #switches matrix list
+             W, #shared weights
+             resp, #response matrix
+             arch, #architecture table
+             lr #learning rate
+            ){
+  deltas=list()
+  gradRaw=list()
+  gradRaw_b=list()
+  W_new=c()
+  b_new=c()
+  weights=list()
+  deltas[[1]]=0
+  gradRaw[[1]]=0
+  gradRaw_b[[1]]=0
+  W_new[[1]]=0
+  b_new[[1]]=0
+  L=length(W)
+  N=nrow(resp)
+  Pool_prev=FALSE
+  for(l in L:2){
+    # print(paste("layer:",l))
+    l_name=colnames(arch)[l-1]
+    if(l==L){
+      deltas[[l]]=Y[[l]]-resp
+      gradRaw[[l]]=t(Y[[l-1]])%*%deltas[[l]]
+      gradRaw_b[[l]]=colSums(deltas[[l]])
+    }
+    if(l<L){
+      # print(l_name)
+      if(l_name=="F"){
+        bp=BP_FC(Y[[l]],Y[[l-1]],deltas[[l+1]],W[[l+1]])
+        deltas[[l]]=bp$deltas
+        gradRaw[[l]]=bp$grads
+        gradRaw_b[[l]]=bp$grads_b
+      }
+      if(l_name=="P" & l<(L-1)){
+        p=arch[1,l-1]
+        if(colnames(arch)[l]=="F")bp1=BP_FC(Y[[l]],Y[[l-1]],deltas[[l+1]],W[[l+1]],TRUE)
+        if(colnames(arch)[l]=="C")bp1=BP_Conv(Y[[l]],Y[[l-1]],deltas[[l+1]],W[[l+1]],arch[,(l-1):l],FALSE,TRUE)
+        deltas1=bp1$deltas
+        bp=BP_Pool(deltas1,M[[l]],p)
+        deltas[[l]]=bp$deltas
+        gradRaw[[l]]=0
+        gradRaw_b[[l]]=0
+      }
+      if(l_name=="C" & l<(L-1)){
+        Pool_prev=ifelse(colnames(arch)[l]=="P",TRUE,FALSE)
+        bp=BP_Conv(Y[[l]],Y[[l-1]],deltas[[l+1]],W[[l+1]],arch[,(l-1):l],Pool_prev,FALSE)
+        deltas[[l]]=bp$deltas
+        gradRaw[[l]]=bp$grads
+        gradRaw_b[[l]]=bp$grads_b
+      }
+    }
+    # gradupdate=gradUpdate(gradRaw[[l]],gradRaw_b[[l]],method="BGD")
+    # gradsW=gradupdate$gradsW
+    # gradsb=gradupdate$gradsb
+    if(l_name=="P")weights[[l]]=0
+    if(l_name!="P"){
+      W_new=W[[l]]$Weight-lr*(1/N)*gradRaw[[l]]
+      b_new=W[[l]]$bias-lr*(1/N)*gradRaw_b[[l]]
+      weights[[l]]=list(Weight=W_new,bias=b_new)
+    }
+  }
+  return(list(W=weights,deltas=deltas,grads=gradRaw,grads_b=gradRaw_b))
+}
+
+#BP on the conv layer
+#Computes deltas & gradients with regards to weights & biases
+
+BP_Conv<-function(Y1,#activation of current layer (compute deltas)
+                  Y2, #activation of previous layer (compute gradients)
+                  deltas, #delta for following layer 
+                  W, #weight from following layer
+                  arch, #architecture details for current and next layers
+                  poolprev, #binary to indicate if previous layer is pooling layer
+                  noWeight=FALSE #binary to indicate if need compute gradients or not
+                 ){
+  
+  arch1=arch[,1]
+  arch2=arch[,2]
+  k1=arch1[1]
+  k2=arch1[2]
+  p1=arch2[3]
+  p2=arch2[4]
+  h=dim(Y1)[1]
+  w=dim(Y1)[2]
+  C=dim(Y2)[3]
+  grads=c()
+  grads_b=c()
+  #compute delta
+  if(poolprev)deltas1=deltas
+  if(!poolprev){
+    b=W$bias
+    Wf=flipKernel(W$Weight)
+    im2col=Im2Col(deltas,Wf,p1,p2,TRUE)
+    X=im2col$X
+    z1=im2col$z1
+    z2=im2col$z2
+    Wf=apply(Wf,3,c)
+    D1=apply(X,3,function(x) x%*%Wf)
+    D1=array(D1,dim=dim(Y1))
+    D1=aperm(D1,c(2,1,3,4))
+    partial_Z=Y1
+    partial_Z[Y1>0]=1
+    deltas1=partial_Z*D1
+  }
+  if(!noWeight){#compute gradient
+    grads1=convolveMat(Y2,deltas1,k1,k2)
+    grads_b=apply(deltas1,3,sum)
+  }
+  return(list(grads=grads,grads_b=grads_b,deltas=deltas1))
+}
+
+#Performs BP on FC layer
+
+BP_FC<-function(Y1,#current activations
+                Y2,#activations for layers l-1
+                deltas, #deltas from layer l+1
+                W, #weights from layer l+1
+                noWeight=FALSE #binary switch indicating if need compute gradients wrt weights
+               ){
+  
+  grads=c()
+  grads_b=c()
+  b=W$bias
+  W=W$Weight
+  deltas1=deltas%*%t(W)
+  partial_Z=Y1
+  partial_Z[Y1>0]=1
+  if(noWeight) deltas1=array(t(deltas1),dim(Y1))
+  deltas1=partial_Z*deltas1
+  if(!noWeight){
+    if(length(dim(Y2))>2) Y2=t(apply(Y2,4,c))
+    grads=t(Y2)%*%deltas1
+    grads_b=colSums(deltas1)
+  }
+  
+  return(list(grads=grads,grads_b=grads_b,deltas=deltas1))
+}
+
+#BP on pooling layer
+#Basically it upscales the gradient to the CONV layers
+BP_Pool<-function(D, #deltas coming from FC or CONV layers
+                  M, #switches indicating position of maximum
+                  p #stride
+                 ){
+  h=dim(D)[1]
+  w=dim(D)[2]
+  d=dim(D)[3]
+  N=dim(D)[4]
+  D1=array(0,dim=c(h*p,w*p,d,N))
+  tab=as.matrix(expand.grid(seq(1,d),seq(1,N)))
+  for(x in 1:nrow(tab)){
+    d0=tab[x,1]
+    n=tab[x,2]
+    for(i in 1:h){
+      for(j in 1:w){
+        i1=1+p*(i-1)
+        j1=1+p*(j-1)
+        Dtmp=D1[i1:(i1+p-1),j1:(j1+p-1),d0,n]
+        id=M[i,j,d0,n]
+        val=D[i,j,d0,n]
+        if(id==1)Dtmp[1,1]=val
+        if(id==2)Dtmp[2,1]=val
+        if(id==3)Dtmp[1,2]=val
+        if(id==4)Dtmp[2,2]=val
+        D1[i1:(i1+p-1),j1:(j1+p-1),d0,n]=Dtmp
+      }
+    }
+  }
+  return(list(deltas=D1))
+}
